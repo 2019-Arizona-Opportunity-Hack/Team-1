14665889360b965adba9ed3a19b2ce29d082e8a7
Phillip Nguyen <pdnguye3@asu.edu>
2019-10-20 02:48:24 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,109 +7,38 @@ const start = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'laptop-gs0tad47'
+        host: '10.2.17.20'
     });
 
     await server.register(require('inert'));
 
-	server.route({
-        method: 'GET',
-        path: '/styles/styles.css',
-        handler: function (request, h) {
-
-            return h.file('styles.css');
-        }
-    });
-	
-	server.route({
-        method: 'GET',
-        path: '/images/shoppingcart.png',
-        handler: function (request, h) {
-
-            return h.file('shoppingcart.png');
-        }
-    });
-
-	server.route({
+		server.route({
         method: 'GET',
         path: '/',
         handler: function (request, h) {
 
-			console.log(request.info.remoteAddress + ' is entering age.html');
-            return h.file('age.html');
-        }
-    });
-	
-	server.route({
-        method: 'GET',
-        path: '/age.html',
-        handler: function (request, h) {
-
-			console.log(request.info.remoteAddress + ' is entering age.html');
-            return h.file('age.html');
+            return h.file('index.html');
         }
     });
 
 	server.route({
-        method: 'GET',
-        path: '/police.html',
-        handler: function (request, h) {
-
-			console.log(request.info.remoteAddress + ' is entering police.html');
-            return h.file('police.html');
-        }
-    });
-
-	server.route({
-        method: 'GET',
-        path: '/address.html',
-        handler: function (request, h) {
-
-			console.log(request.info.remoteAddress + ' is entering address.html');
-            return h.file('address.html');
-        }
-    });
-
-	server.route({
-        method: 'GET',
-        path: '/help.html',
-        handler: function (request, h) {
-
-			console.log(request.info.remoteAddress + ' is entering help.html');
-            return h.file('help.html');
-        }
-    });
-
-    server.route({
         method: 'GET',
         path: '/index.html',
         handler: function (request, h) {
 
-			console.log(request.info.remoteAddress + ' is entering index.html');
             return h.file('index.html');
         }
-    });	
+    });
 	
 	server.route({
         method: 'GET',
-        path: '/checkout.html',
+        path: '/*',
         handler: function (request, h) {
 
-			console.log(request.info.remoteAddress + ' is entering checkout.html');
-            return h.file('checkout.html');
+            return 'ERROR 404. PAGE NOT FOUND';
         }
     });
-
-	server.route({
-        method: 'GET',
-        path: '/waiting.html',
-        handler: function (request, h) {
-
-			console.log(request.info.remoteAddress + ' is entering waiting.html');
-            return h.file('waiting.html');
-        }
-    });
-
+	
     await server.start();
 
     console.log('Server running at:', server.info.uri);
